91885a39048cb04bc73240a99a7816e39ae545d8
Mariam Abdelrazek <abdelrazekmariam324@gmail.com>
2025-06-30 10:43:05 +0300

    bug: fix import of the contact form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,9 +7,9 @@ import OurProjects from "./_components/OurProjects/OurProjects";
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import OurServices from "./_components/OurServices/OurServices";
-import Contact from "./Contact/Contact";
 import Reviwes from "./_components/Reviwes/Reviwes";
 import Footer from "./_components/Footer/Footer";
+import Contact from "./contact/Contact";
 config.autoAddCss = false;
 
 
